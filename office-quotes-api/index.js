98b1ab88769249c644db3df7fbce1b01b7dbceda
Jon Pesta <jon.pesta@gmail.com>
2022-08-25 20:41:15 -0400

    Removed inspiration code.

--- office-quotes-api/index.js
+++ office-quotes-api/index.js
@@ -1,49 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 
-// const data = {
-//   "beasts": [
-//     {
-// 	    "id": "md",
-// 	    "legs": 6,
-// 	    "binomial": "Musca domestica",
-// 	    "commonName": "housefly",
-//     },
-//     {
-// 	    "id": "nr",
-// 	    "legs": 8,
-// 	    "binomial": "Neriene radiata",
-// 	    "commonName": "filmy dome spider",
-//     },
-//     {
-// 	    "id": "cc",
-// 	    "legs": 2,
-// 	    "binomial": "Corvus corone",
-// 	    "commonName": "carrion crow",
-//     },
-//     {
-// 	    "id": "fc",
-// 	    "legs": 4,
-// 	    "binomial": "Felis catus",
-// 	    "commonName": "cat",
-//     }
-//   ]
-// };
-
 const prisma = new PrismaClient();
-
-// const typeDefs = gql`
-// 	type Beast {
-// 		id: ID
-// 		legs: Int
-// 		binomial: String
-// 		commonName: String
-// 	}
-
-// 	type Query {
-// 		beasts: [Beast]
-// 	}
-// `;
 
 const typeDefs = gql`
 	type Quote {
@@ -59,8 +17,6 @@ const typeDefs = gql`
 
 const resolvers = {
 	Query: {
-		// Returns array of all beasts.
-		// beasts: () => data.beasts
         quotes: () => prisma.quote.findMany()
 	}
 };
